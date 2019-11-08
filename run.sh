0d9584546d21a197202ce733579f1f7b57fc13bf
KaiqueSousa <kaique.una@gmail.com>
2019-11-08 16:00:39 -0300

    Modificações

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 docker build -t dac-persist/postgres ./postgres  
-docker run -p 5433:5432 --name postgres -d dac-persist/postgres
+docker run -d -p 5433:5432 --name postgres -d dac-persist/postgres
 
 mvn clean package                      
 
 chmod -R 777 ./target
 docker build -t dac-persist/app .         
-docker run -d -p 8080:8080 --name app --link persist:host-container dac-persist/app
+docker run -d -p 8080:8080 --name app --link postgres:host-container dac-persist/app
 docker logs -f app
 docker logs -f postgres
+
+
+
+
+
 
 
 
--- stop.sh
+++ stop.sh
@@ -1,7 +1,13 @@
-dokcer stop app            
+docker stop app            
 docker rm app              
 docker rmi -f dac-persist/app   
 
 docker stop postgres           
 docker rm postgres             
 docker rmi -f dac-persist/postgres  
+
+
+
+
+
+
